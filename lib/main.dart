1d8680b268701c683f7d6806e8a631730ca63a5c
Eugene <eugenemahota@gmail.com>
2021-07-27 22:42:27 +0300

    remove test module

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo',
+      title: 'FIXPhoto',
       theme: AppTheme().defaultTheme,
       initialRoute: '/splash-screen',
       routes: {
--- lib/shared/services/core/service_locator.dart
+++ lib/shared/services/core/service_locator.dart
@@ -5,19 +5,15 @@ import 'package:tirmobile/shared/services/auth_service.dart';
 import 'package:tirmobile/shared/services/tasks_service.dart';
 import 'package:tirmobile/task-module/task_view_model.dart';
 import 'package:tirmobile/tasks-module/tasks_view_model.dart';
-import 'package:tirmobile/users-module/users_service.dart';
-import 'package:tirmobile/users-module/users_view_model.dart';
 
 GetIt serviceLocator = GetIt.instance;
 
 void setupServiceLocator() {
   /** services */
   serviceLocator.registerLazySingleton<WebApi>(() => WebApiService());
-  serviceLocator.registerLazySingleton<UsersService>(() => UsersService());
   serviceLocator.registerLazySingleton<AuthService>(() => AuthService());
   serviceLocator.registerLazySingleton<TasksService>(() => TasksService());
   /** view models */
-  serviceLocator.registerFactory<UsersViewModel>(() => UsersViewModel());
   serviceLocator.registerFactory<LoginViewModel>(() => LoginViewModel());
   serviceLocator.registerFactory<TasksViewModel>(() => TasksViewModel());
   serviceLocator.registerFactory<TaskViewModel>(() => TaskViewModel());
